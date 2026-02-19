d10b4614ad10331162c98e3fb1e0a7bc341f313a
Amer0202 <omer.bulugtekin@gmail.com>
2026-02-19 23:21:08 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'falciana-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './assets/icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
